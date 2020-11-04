f44d46173a3114489e22ca1b90bd9e06a9b32a2c
Remy <remygomes@outlook.com>
2020-11-04 11:26:44 +0100

    :construction: Application de livres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'livre';
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyD7zde8-vtq3boqJNG0bCUws_5iHpfpOFU",
+      authDomain: "book-angular-33cad.firebaseapp.com",
+      databaseURL: "https://book-angular-33cad.firebaseio.com",
+      projectId: "book-angular-33cad",
+      storageBucket: "book-angular-33cad.appspot.com",
+      messagingSenderId: "525715770797",
+      appId: "1:525715770797:web:a6ea0b068b48f0727aa524",
+      measurementId: "G-B3GTMTG0G1"
+    }
+    firebase.initializeApp(config);
+  }
 }
